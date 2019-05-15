ebead9fa23021477ef1e68cae9d6675437e4f2ad
Terje Sandstrom <terje@hermit.no>
2019-05-15 17:52:19 +0200

    Initial code

--- OneTimeTearDown/Class1.cs
+++ OneTimeTearDown/Class1.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace OneTimeTearDown
 {
     public class Class1
     {
+        [Test]
+        public void Whatever()
+        {
+
+        }
+
+        [OneTimeTearDown]
+        public void EndIt()
+        {
+            Assert.Fail();
+        }
     }
 }
